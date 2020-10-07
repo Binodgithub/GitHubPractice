251b371104f0afdbaf48147e50724dc7388bac2a
Binod <donibkhadka@gmail.com>
2020-10-07 16:41:33 -0400

    Binod is commiting

--- GitHub/src/GitHubPractice/GitHubClass.java
+++ GitHub/src/GitHubPractice/GitHubClass.java
@@ -3,6 +3,7 @@ package GitHubPractice;
 public class GitHubClass {
 	public static void main(String[] args) {
 		System.out.println("My name is Binod khadka");
+		System.out.println("Binam is my son");
 	}
 
 }
