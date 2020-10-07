1d2fd10829533f36d13cf497509f1fc830e34e87
Binod <donibkhadka@gmail.com>
2020-10-07 17:36:06 -0400

    Nirmala

--- GitHub/src/GitHubPractice/GitHubClass.java
+++ GitHub/src/GitHubPractice/GitHubClass.java
@@ -4,6 +4,10 @@ public class GitHubClass {
 	public static void main(String[] args) {
 		System.out.println("My name is Binod khadka");
 		System.out.println("Binam is my son");
+		System.out.println("Bye Bye");
+		
 	}
+	
+	
 
 }
